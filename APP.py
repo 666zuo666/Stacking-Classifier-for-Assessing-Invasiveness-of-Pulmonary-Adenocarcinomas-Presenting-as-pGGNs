a03859f63a63850d3d86dc8b4dc208aa646a3c76
666zuo666 <165533320+666zuo666@users.noreply.github.com>
2025-01-09 19:37:01 +0800

    Update APP.py

--- APP.py
+++ APP.py
@@ -33,8 +33,8 @@ if predict_button:
     st.header("Prediction result")
     try:
         # 将输入特征转换为模型所需格式
-        input_array = np.array(Radscore,Size,CT_value,Age)
-        #input_array = np.array(Radscore,Size,CT_value,Age).reshape(1, -1)
+        #input_array = np.array(Radscore,Size,CT_value,Age)
+        input_array = np.array(Radscore,Size,CT_value,Age).reshape(1, -1)
 
         # 模型预测
         prediction = stacking_regressor.predict(input_array)[0]
